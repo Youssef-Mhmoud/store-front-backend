85250866d0b9aab6d9368edb53b04a6c0039df02
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2022-10-30 16:49:26 +0200

    add user models

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import UserModel from '../models/user.model';
 
 const userModel = new UserModel();
@@ -15,3 +15,67 @@ export const create = async (req: Request, res: Response) => {
     console.log(error);
   }
 };
+
+export const getMany = async (
+  _: Request,
+  res: Response,
+) => {
+  try {
+    const users = await userModel.getMany();
+    res.json({
+      status: 'success',
+      data: users,
+      message: 'User retrieved successfuly',
+    });
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const getOne = async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    const user = await userModel.getOne(req.params.id as unknown as string);
+    res.json({
+      status: 'success',
+      data: user,
+      message: 'User retrieved successfuly',
+    });
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const updateOne = async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    const user = await userModel.updateOne(req.body);
+    res.json({
+      status: 'success',
+      data: user,
+      message: 'User Updated successfuly',
+    });
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const deleteOne = async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    const user = await userModel.deleteOne(req.params.id as unknown as string);
+    res.json({
+      status: 'success',
+      data: user,
+      message: 'User Deleted successfuly',
+    });
+  } catch (error) {
+    console.log(error)
+  }
+}
--- src/routes/api/user.routes.ts
+++ src/routes/api/user.routes.ts
@@ -3,6 +3,10 @@ import * as controllers from '../../controllers/users.controllers';
 
 const routes = Router();
 
-routes.post('/',  controllers.create);
-
+routes.route('/').get(controllers.getMany).post(controllers.create);
+routes
+  .route('/:id')
+  .get(controllers.getOne)
+  .patch(controllers.updateOne)
+  .delete(controllers.deleteOne);
 export default routes;
